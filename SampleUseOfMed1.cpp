487496efa7db8a7add02a45f8cdb4dc2ed57d241
tjxafa <73420919+tjxafa@users.noreply.github.com>
2023-05-06 00:04:41 +0800

    Update SampleUseOfMed1.cpp

--- SampleUseOfMed1.cpp
+++ SampleUseOfMed1.cpp
@@ -5,5 +5,7 @@ int main() {
 
     bool result = playMed1();
 
+    // the function playMed1() will return true if the player wins, and return false if the player fails. thus bool result can store the result of game tic-tac-toc
+    
     return 0;
 }
